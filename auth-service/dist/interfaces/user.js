06edd7804b54a9fdc1f5f748b054874d21f89d2e
Roma Yarmolenko <yrm1703@gmail.com>
2025-09-15 18:18:27 +0300

    finish commit

--- auth-service/dist/interfaces/user.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Roles = void 0;
-var Roles;
-(function (Roles) {
-    Roles["USER"] = "user";
-    Roles["ADMIN"] = "admin";
-    Roles["SUPERADMIN"] = "superAdmin";
-})(Roles || (exports.Roles = Roles = {}));
--- project-service/dist/interfaces/profile.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ProfilePermissions = void 0;
-var ProfilePermissions;
-(function (ProfilePermissions) {
-    ProfilePermissions["HIGH"] = "high";
-    ProfilePermissions["MEDIUM"] = "medium";
-    ProfilePermissions["LOW"] = "low";
-})(ProfilePermissions || (exports.ProfilePermissions = ProfilePermissions = {}));
